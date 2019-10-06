d88355cac2cb6987676e57ab4041c6e9b953a1d9
1505523898@qq.com <H19990727769x>
2019-10-06 23:21:07 +0800

    add

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@ image_version=`date +%Y%m%d%H%M`;
 echo $image_version;
 # cd vue2docker
 git pull --rebase origin master;
-docker stop vue2docker;
-docker rm vue2docker;
-docker build -t vue2docker:$image_version .;
+docker stop vant-MT;
+docker rm vant-MT;
+docker build -t vant-MT:$image_version .;
 docker images;
-docker run -p 10002:80 -d --name vue2docker vue2docker:$image_version;
+docker run -p 10002:80 -d --name vant-MT vant-MT:$image_version;
 # -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
-docker logs vue2docker;
+docker logs vant-MT;
 #删除build过程中产生的镜像    #docker image prune -a -f
 docker rmi $(docker images -f "dangling=true" -q)
